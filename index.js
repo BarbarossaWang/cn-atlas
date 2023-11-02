c6967125c8c65ed67961595ddff7438832917ce7
amll <jiewangcn@outlook.com>
2023-11-02 10:24:50 +0800

    chore

---
+++ index.js
@@ -0,0 +1,7 @@
+import cnAtlas from './cn-atlas.json'
+import nation from './nation.json'
+import provinces from './provinces.json'
+import prefectures from './prefectures.json'
+
+export default cnAtlas
+export {nation, provinces, prefectures}
